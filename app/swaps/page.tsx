27c578605745077f1848ad223ed46c54a5d1318d
ttang <ttang@ttang-MacBook-Pro-2021.local>
2024-03-19 16:21:32 +0800

    log metadata

--- app/swaps/page.tsx
+++ app/swaps/page.tsx
@@ -2,9 +2,9 @@ import { getFrameMetadata } from "frog/next";
 import type { Metadata } from "next";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const frameTags = await getFrameMetadata(
-    `${process.env.VERCEL_URL}/swaps/api`
-  );
+  const metadataPage = `${process.env.VERCEL_URL}/swaps/api`;
+  const frameTags = await getFrameMetadata(metadataPage);
+  console.log("metadataPage", metadataPage);
   console.log("frameTags", frameTags);
   return {
     other: frameTags,
