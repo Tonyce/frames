ff789ee3a1830df29b9561f9ae9702d97bff330b
ttang <ttang@ttang-MacBook-Pro-2021.local>
2024-03-21 00:45:42 +0800

    address fix

--- app/swapsv2/api/[[...routes]]/frame/styles.ts
+++ app/swapsv2/api/[[...routes]]/frame/styles.ts
@@ -1,31 +1,31 @@
 export const backgroundStyles: Hono.CSSProperties = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
   padding: 64,
   gap: 48,
-  backgroundColor: '#FCFCFD',
-  color: '#22262A',
+  backgroundColor: "#FCFCFD",
+  color: "#22262A",
   lineHeight: 1.2,
   fontSize: 50,
-  width: '100%',
-  height: '100%',
-  fontFamily: 'inter',
-}
+  width: "100%",
+  height: "100%",
+  fontFamily: "inter",
+};
 
 export const titleStyles: Hono.CSSProperties = {
-  fontSize: 106,
-  fontFamily: 'gilroy',
-  textAlign: 'center',
+  fontSize: 86,
+  fontFamily: "gilroy",
+  textAlign: "center",
   lineHeight: 1,
-}
+};
 
 export const warningStyles: Hono.CSSProperties = {
-  position: 'absolute',
+  position: "absolute",
   bottom: 20,
-  color: '#5E6773',
+  color: "#5E6773",
   fontSize: 28,
-  textAlign: 'center',
+  textAlign: "center",
   lineHeight: 1.3,
-}
+};
--- app/swapsv2/api/[[...routes]]/frame/token-selection.tsx
+++ app/swapsv2/api/[[...routes]]/frame/token-selection.tsx
@@ -29,13 +29,13 @@ export const assets = [
     name: "$MEMBER",
     network: "base",
     image: "https://i.imgur.com/GobFUtr.png",
-    address: "0xA4eFE9e8E2A2D5A2aC46805f233b8e49d0e11955",
+    address: "0x7d89E05c0B93B24B5Cb23A073E60D008FEd1aCF9",
   },
   {
     name: "$WOWOW",
     network: "base",
     image: "https://i.imgur.com/MEQ9ndF.jpeg",
-    address: "0x8118e4b58451064303034bcd3a0b0f2e8dd45ec3",
+    address: "0xB36A0e830bD92E7AA5D959c17A20D7656976dd98",
   },
   {
     name: "$TN100x",
@@ -49,7 +49,7 @@ export const assets = [
     network: "base",
     image:
       "https://pbs.twimg.com/profile_images/1549962330368270336/Ymfk7XtM_400x400.jpg",
-    address: "0xae2ce200bdb67c472030b31f602f0756c9aeb61c",
+    address: "0xAfb89a09D82FBDE58f18Ac6437B3fC81724e4dF6",
   },
 ];
 
@@ -102,6 +102,7 @@ export const tokenSelectionScreen = (c: CustomFrameContext) => {
             </div>
           ))}
         </div>
+        <div>0 will be default if the number not exist</div>
       </div>
     ),
     intents: [
