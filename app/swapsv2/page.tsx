b187383bfd4a78adf2e0d618a7eb9b27b815be73
ttang <ttang@ttang-MacBook-Pro-2021.local>
2024-03-21 00:56:39 +0800

    swapsv2 api

--- app/swapsv2/page.tsx
+++ app/swapsv2/page.tsx
@@ -2,7 +2,7 @@ import { getFrameMetadata } from "frog/next";
 import type { Metadata } from "next";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const metadataPage = `${process.env.WEBSITE_URL}/swaps/api`;
+  const metadataPage = `${process.env.WEBSITE_URL}/swapsv2/api`;
   const frameTags = await getFrameMetadata(metadataPage);
   // console.log("metadataPage", metadataPage);
   // console.log("frameTags", frameTags);
